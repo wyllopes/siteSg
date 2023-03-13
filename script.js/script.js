2ef2fbd8a3eebc0d0902bcdbcd5bd5425f9b6644
Wilson lopes <wyllopes@gmail.com>
2023-03-13 20:11:57 -0300

    Update script.js

--- script.js/script.js
+++ script.js/script.js
@@ -2,6 +2,7 @@ const hamburguer = document.querySelector(".hamburguer");
 const navmenu = document.querySelector(".nav-menu");
 
 hamburguer.addEventListener("click", () => {
+
     hamburguer.classList.toggle("active");
     navmenu.classList.toggle("active");
 })
